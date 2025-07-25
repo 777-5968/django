b9f3dd3948d9ece267ebd2ec4f7054eac9a88561
J0khimCode <jkitavi21086@gmail.com>
2025-07-25 14:26:09 +0300

    first commit

--- first_app/urls.py
+++ first_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .import views
 urlpatterns = [
     path('',views.index,name='index'),
-    path('safari/',views.safari,name='safari'),
-    path('home/',views.home,name='home')
+    path('home/',views.home,name='home'),
+    path('about/',views.about,name='about us'),
+    path('contact/',views.contact,name='contact us')
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -4,9 +4,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello,world")
-def safari(request):
-    return HttpResponse("Hello,safari")
+    return HttpResponse("hello world")
 def home(request):
     return render (request,'home.html')
-
+def about(request):
+    return render(request,'about us.html')
+def contact(request):
+    return render(request,'contact.html')
